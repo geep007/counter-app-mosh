8a2c59073c44c0cbd3ccfa0f4b3e3e88ad04426d
geep007 <geetparmar001@gmail.com>
2020-07-28 23:13:51 +0530

    Updates

---
+++ precache-manifest.56cdc9828983328f7cf5caf073ed04a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d8edcbd000ebde74a3e976a8becce0",
+    "url": "/counter-app/index.html"
+  },
+  {
+    "revision": "baeed1d1286e3dd1c87d",
+    "url": "/counter-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8866713584bcda973f17",
+    "url": "/counter-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "baeed1d1286e3dd1c87d",
+    "url": "/counter-app/static/js/2.989a5cbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter-app/static/js/2.989a5cbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8866713584bcda973f17",
+    "url": "/counter-app/static/js/main.a0af05d1.chunk.js"
+  },
+  {
+    "revision": "8d3c91def09e322d68af",
+    "url": "/counter-app/static/js/runtime-main.7c4e8537.js"
+  }
+]);
